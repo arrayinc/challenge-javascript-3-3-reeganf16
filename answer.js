05e037d33a96016ebb2ea8dd9febe9c2bc9cb213
reeganford <rmford1695@gmail.com>
2019-12-05 14:27:40 -0700

    Finished challenge #3

--- answer.js
+++ answer.js
@@ -1,1 +1,14 @@
 // *** YOUR ANSWER BELOW ***
+
+function divisibleBy(x,y){
+    for(let i = 100; i > 0; i--) {
+        if ((i % x === 0) && (i % y !==0)){
+            console.log(i);
+        }
+        if ((i % y === 0) && (i % x !== 0)) {
+            console.log(i);
+        }
+    }
+}
+
+(divisibleBy(6,7));
